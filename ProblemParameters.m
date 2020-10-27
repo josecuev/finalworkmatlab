b6f69e1a14b824621599ca3dc1012ccac8dfffe1
Jose <jmcuev89@gmail.com>
2020-10-27 12:19:45 -0300

    Primer resultado coherente hallado

--- ProblemParameters.m
+++ ProblemParameters.m
@@ -1,10 +1,11 @@
-function [a,b,F,E,v] = ProblemParameters()
+function [a,b,F,E,v, Lc] = ProblemParameters()
 
 %problem data parameters definition
-a=0.3; % internal radius m
-b=0.4; % external radius m
+a=0.2; % internal radius m
+b=0.3; % external radius m
 F=3000;  % Pressure in N/m^2
 E=2e11; %Young Modulus N/m^2
 v=0.26; % Poisson's coificient
+Lc=3; %Number of elements in r direction
 
 end
