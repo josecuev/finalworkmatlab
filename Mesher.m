a8ac37667a585d9c2a0ac7c5de4b05ef9e30ad0f
Jose <jmcuev89@gmail.com>
2020-10-28 20:28:38 -0300

    Q8 conectivity hecho

--- Mesher.m
+++ Mesher.m
@@ -1,4 +1,9 @@
-function [InmputFileName]=Mesher(Lc)
+%function [InmputFileName]=Mesher(Lc,Q)
+
+
+
+Lc=2;
+Q=8;
 
 [a, b, F, E, v] = ProblemParameters();
 
@@ -8,7 +13,7 @@ G=E/(2*(1+v));
 %Defines a ratio of the elements based on the geometry
 er=round((pi*(a+b)/4)/(b-a));
 
-%Number of elements in r and theta direction.
+%Number of elements in r and theta direction
 Lr=Lc;
 Ltheta=round(Lr*er);
 L=Lr*Ltheta;
@@ -30,10 +35,6 @@ THETA=reshape(THETA,[],1); % convert matrix to column vector
 X=R.*cos(THETA);
 Y=R.*sin(THETA);
 
-COORDS=[X,Y];
-
-%Plot the nodes
-%scatter(X,Y)
 
 
 %Generate mesh conectivity info
@@ -43,37 +44,171 @@ con2=zeros(L,1);
 con3=zeros(L,1);
 con4=zeros(L,1);
 con5=zeros(L,1);
+con6=zeros(L,1);
+con7=zeros(L,1);
+con8=zeros(L,1);
+con9=zeros(L,1);
+
+
+%If the element is cuadratic, inicialize some variables
+if Q==8
+    Nm=Lr*Ntheta+Ltheta*Nr
+    midPointMap=containers.Map;
+    l=N+1;
+end
+
+
 
 k=1;
 for i = 1 : Lr
     for j = 1 : Ltheta
+        
         con1(k)=k;
         con2(k)=Ntheta*(i-1)+j;
         con3(k)=Ntheta*(i-1)+j+1;
         con4(k)=Ntheta*(i)+j+1;
         con5(k)=Ntheta*(i)+j;
+        
+        %If Q=8 add midpoints nodes
+        if Q==8
+            
+            key1=append(string(con3(k)),"-",string(con2(k)));
+            key2=append(string(con4(k)),"-",string(con3(k)));
+            key3=append(string(con5(k)),"-",string(con4(k)));
+            key4=append(string(con2(k)),"-",string(con5(k)));
+            
+            %Check if the midpoint was previously added, otherwhise, create
+            %a new entry in the mapping
+            
+            %new node in face 1
+            if isKey(midPointMap,key1)
+                %uses the previously created node and create the
+                %conectitivity info
+                con6(k)=midPointMap(key1);
+            else
+                
+                %create the entry key
+                newkey=append(string(con2(k)),"-",string(con3(k)));
+                
+                %calculate the midpoint in polar
+                R(l)=(R(con2(k))+R(con3(k)))/2;
+                THETA(l)=(THETA(con2(k))+THETA(con3(k)))/2;
+                %transoform into rectangular
+                X(l)=R(l).*cos(THETA(l));
+                Y(l)=R(l).*sin(THETA(l));
+                
+                %Store in the container
+                midPointMap(newkey)=l;
+                
+                %create the conectivity info
+                con6(k)=midPointMap(newkey);
+                l=l+1;
+            end
+            
+            %new node in face 2
+            if isKey(midPointMap,key2)
+                %uses the previously created node and create the
+                %conectitivity info
+                con7(k)=midPointMap(key2);
+            else
+                %create the entry key
+                newkey=append(string(con3(k)),"-",string(con4(k)));
+                
+                %calculate the midpoint in polar
+                R(l)=(R(con3(k))+R(con4(k)))/2;
+                THETA(l)=(THETA(con3(k))+THETA(con4(k)))/2;
+                %transoform into rectangular
+                X(l)=R(l).*cos(THETA(l));
+                Y(l)=R(l).*sin(THETA(l));
+                
+                %Store in the container
+                midPointMap(newkey)=l;
+                
+                %create the conectivity info
+                con7(k)=midPointMap(newkey);
+                l=l+1;
+            end
+            
+            %new node in face 3
+            if isKey(midPointMap,key3)
+                %uses the previously created node and create the
+                %conectitivity info
+                con8(k)=midPointMap(key3);
+            else
+                %create the entry key
+                newkey=append(string(con4(k)),"-",string(con5(k)));
+                
+                %calculate the midpoint in polar
+                R(l)=(R(con4(k))+R(con5(k)))/2;
+                THETA(l)=(THETA(con4(k))+THETA(con5(k)))/2;
+                %transoform into rectangular
+                X(l)=R(l).*cos(THETA(l));
+                Y(l)=R(l).*sin(THETA(l));
+                
+                %Store in the container
+                midPointMap(newkey)=l;
+                
+                %create the conectivity info
+                con8(k)=midPointMap(newkey);
+                l=l+1;
+            end
+            
+            %new node in face 4
+            if isKey(midPointMap,key4)
+                %uses the previously created node and create the
+                %conectitivity info
+                con9(k)=midPointMap(key4);
+            else
+                %create the entry key
+                newkey=append(string(con5(k)),"-",string(con2(k)));
+                
+                %calculate the midpoint in polar
+                R(l)=(R(con5(k))+R(con2(k)))/2;
+                THETA(l)=(THETA(con5(k))+THETA(con2(k)))/2;
+                %transoform into rectangular
+                X(l)=R(l).*cos(THETA(l));
+                Y(l)=R(l).*sin(THETA(l));
+                
+                %Store in the container
+                midPointMap(newkey)=l;
+                
+                %create the conectivity info
+                con9(k)=midPointMap(newkey);
+                l=l+1;
+            end
+        end
+      
         k=k+1;
     end
 end
+
+
+COORDS=[X,Y];
+
+%Plot the nodes
+scatter(X,Y)
+
 
 InmputFileName=sprintf('CurvedCantilever_Lc=%d.txt',Lc)
 fileID = fopen(InmputFileName,'w');
 fprintf(fileID,'No._material_props:    3\n');
 fprintf(fileID,'    Shear_modulus:   %0.2f\n', G);
 fprintf(fileID,'    Poissons_ratio:  %0.2f\n', v);
-fprintf(fileID,'    Plane_strain/stress: 1\n');
+fprintf(fileID,'    Plane_strain/stress: 0\n'); %zero for plane stress
 fprintf(fileID,'No._coords_per_node:   2\n');
 fprintf(fileID,'No._DOF_per_node:      2\n');
-fprintf(fileID,'No._nodes:             %d\n', N);
+fprintf(fileID,'No._nodes:             %d\n', N+Nm);
 fprintf(fileID,'Nodal_coords:\n');
-for i= 1 : N
+
+[m,n] = size(COORDS);
+for i= 1 : m
     fprintf(fileID,'%0.4f\t%0.4f\n',COORDS(i,1),COORDS(i,2));
 end
 fprintf(fileID,'No._elements:                       %d\n', L);
-fprintf(fileID,'Max_no._nodes_on_any_one_element:   4\n');
+fprintf(fileID,'Max_no._nodes_on_any_one_element:   %d\n', Q);
 fprintf(fileID,'element_identifier; no._nodes_on_element; connectivity:\n');
 for i= 1 : L
-    fprintf(fileID,'%d\t4\t%d\t%d\t%d\t%d\n',con1(i),con2(i),con3(i),con4(i),con5(i));
+    fprintf(fileID,'%d\t4\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n',con1(i),con2(i),con3(i),con4(i),con5(i),con6(i),con7(i),con8(i),con9(i));
 end
 fprintf(fileID,'No._nodes_with_prescribed_DOFs:  %d\n', 2*Nr);
 fprintf(fileID,'Node_#, DOF#, Value:\n');
@@ -89,7 +224,7 @@ end
 
 fclose(fileID);
 
-end
+%end
 
 
 
--- Part1_2_3.m
+++ Part1_2_3.m
@@ -2,13 +2,13 @@ clear
 clc
 
 %Mesh the problem
-inputfilename=Mesher(3);
+%inputfilename=Mesher(3);
 
 %Solve the meshed problem for linear interpolation functions:
-FEM_2Dor3D_linelast_standard(inputfilename);
+FEM_2Dor3D_linelast_standard("Linear_elastic_quad8.txt");
 
 %Solve the meshed problem for cuadratic interpolation functions:
-FEM_2Dor3D_linelast_standard(inputfilename);
+%FEM_2Dor3D_linelast_standard(inputfilename);
 
 
 
